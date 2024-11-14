74e47342116c55e21fbea11abd8c4646a1058adf
x3a1n4 <billybobbyjoeyyay@gmail.com>
2024-11-14 14:33:03 -0500

    Fix signal connecting

--- my_game.gd
+++ my_game.gd
@@ -17,7 +17,7 @@ func _ready() -> void:
 		moneybag.position.x = randf_range(30, 560)
 		moneybag.position.y = randf_range(30, 290)
 		
-		moneybag.player_picked_up.connect(_on_player_picked_up_bag()) #don't work
+		moneybag.player_picked_up.connect(_on_player_picked_up_bag) #don't work
 
 func _on_goblin_killed_player() -> void:
 	lose_game.emit()
