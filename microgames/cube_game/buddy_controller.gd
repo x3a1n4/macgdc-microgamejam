3a2a9b4aa82af3278242bc7c8c99466ec5c2f3fe
Alex <44590809+x3a1n4@users.noreply.github.com>
2024-11-26 01:31:30 -0500

    Finish MVP of game, still needs sound effects and  debugging issue where it does not jump to correct grid spot

--- microgames/cube_game/buddy_controller.gd
+++ microgames/cube_game/buddy_controller.gd
@@ -5,24 +5,35 @@ const JUMP_VELOCITY = 4.5
 
 signal worldrotate
 
+var input_dir : Vector2 = Vector2.ZERO
+
+var jump_start_pos : Vector3
+
+@export var grid : GridMap
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		pass
 	
-	if is_on_floor()
-		# play jump_land
-		$AnimationPlayer.play("Jump_Land")
+	if is_on_floor():
+		if $AnimationPlayer.current_animation == "Jump_Idle" or $AnimationPlayer.current_animation == "Jump":
+			# play jump_land
+			$AnimationPlayer.stop()
+			$AnimationPlayer.play("Jump_Land")
 		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("keyboard_left", "keyboard_right", "keyboard_up", "keyboard_down")
+	var last_input_dir = input_dir
+	input_dir = Input.get_vector("keyboard_left", "keyboard_right", "keyboard_up", "keyboard_down")
+	
+	# see if we can jump
+	var jump : bool = input_dir != Vector2.ZERO and input_dir != last_input_dir and $FloorCast.is_colliding()
 	# TODO: handle buffering
-	if input_dir != Vector2.ZERO and is_on_floor():
-		var direction : Vector2i = input_dir.normalized().snapped(Vector2.ONE) # snap to UP, DOWN, LEFT, RIGHT
-		var direction3 = Vector3(direction.x, direction.y, 0)
-		# cast ray in input dir
-		$RayCast3D.target_position = direction3
+	if jump:
+		var direction : Vector2i = input_dir.normalized().rotated(PI).snapped(Vector2.ONE) # snap to UP, DOWN, LEFT, RIGHT
+		var direction3 = Vector3(direction.x, 0, direction.y) # I will never remember that y is vertical here
 		
 		# face mesh in direction
 		look_at(global_transform.origin + direction3, Vector3.UP)
@@ -31,10 +42,43 @@ func _physics_process(delta: float) -> void:
 		$AnimationPlayer.play("Jump")
 		
 		# if ray hit something, face that direction and rotate world
-		if $RayCast3D.is_colliding():
-			worldrotate.emit(direction)
+		$ForewardsCast.force_raycast_update()
+		$ForewardsCast/DownwardsCast.force_raycast_update()
+		if $ForewardsCast.is_colliding():
+			print("rotate up")
+			worldrotate.emit(direction, false)
+		elif not $ForewardsCast/DownwardsCast.is_colliding():
+			print("rotate down")
+			worldrotate.emit(-direction, true)
+			
+		# else, just jump in that direction
+		else:
+			$JumpPath/JumpTimer.start()
+			jump_start_pos = position
+		
+		#position = $FloorCast.get_collision_point() + Vector3.UP/2
+		#snap_to_grid()
+	
+	# check if we're jumping
+	if not $JumpPath/JumpTimer.is_stopped():
+		# set jump position
+		var jump_progress = 1 - $JumpPath/JumpTimer.time_left / $JumpPath/JumpTimer.wait_time
+		
+		$JumpPath/PathFollow3D.set_progress_ratio(jump_progress) 
+		# position is not updated in time
+		position = jump_start_pos + $JumpPath/PathFollow3D.get_position().rotated(Vector3.UP, rotation.y)
 
 	move_and_slide()
+
+func _on_jump_timer_timeout() -> void:
+	$JumpPath/PathFollow3D.set_progress_ratio(0) # hack due to https://github.com/godotengine/godot/issues/95612
+
+func _on_rotation_timer_timeout() -> void:
+	snap_to_grid()
+
+func snap_to_grid():
+	var grid_pos = grid.local_to_map(grid.to_local(position))
+	position = grid.to_global(grid.map_to_local(grid_pos))
 
 # pseudo animation tree
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
@@ -43,7 +87,7 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 		# play jump idle
 		$AnimationPlayer.play("Jump_Idle")
 	# if jump
-	if anim_name == "Jump_Idle":
+	if anim_name == "Jump_Land":
 		# play idle
 		$AnimationPlayer.play("Idle")
 	pass # Replace with function body.
--- microgames/cube_game/cube_game.gd
+++ microgames/cube_game/cube_game.gd
@@ -1,6 +1,7 @@
 extends Microgame
 
 @export var num_cubes = 10
+@export var explosion : PackedScene
 
 var directions = [
 	Vector3i.UP,
@@ -16,6 +17,7 @@ func _ready() -> void:
 	super()
 	
 	# spawn cubes
+	# note: generation is almost for sure not working
 	for i in range(num_cubes):
 		var used_cells : Array[Vector3i] = $SubViewport/GridMap.get_used_cells_by_item(ITEMNUM)
 		
@@ -35,14 +37,82 @@ func _ready() -> void:
 			if cell in used_cells:
 				adjacent_cells.erase(cell)
 			
-			if cell.length() < 5:
+			if cell.length() > 7:
 				adjacent_cells.erase(cell) # bit too far, don't add box here
 		
 		var position = adjacent_cells.pick_random()
 		$SubViewport/GridMap.set_cell_item(position, ITEMNUM)
 	
 	# spawn buddy and flag
+	var buddy_spawn = get_buddy_spawn($SubViewport/GridMap.get_used_cells_by_item(ITEMNUM))
+	$SubViewport/Buddy.position = $SubViewport/GridMap.map_to_local(buddy_spawn) + Vector3.UP * $SubViewport/GridMap.cell_size.z / 2
+	
+	var flag_spawn = get_flag_spawn($SubViewport/GridMap.get_used_cells_by_item(ITEMNUM))
+	$SubViewport/GridMap/Flag.position = $SubViewport/GridMap.map_to_local(flag_spawn) + Vector3.UP * $SubViewport/GridMap.cell_size.z / 2
 
+func get_flag_spawn(cells : Array[Vector3i]) -> Vector3i:
+	var output = cells.reduce(func(max, vec): return vec if -vec.x+vec.y/10-vec.z > -max.x+max.y/10-max.z else max)
+	return output
+	
+func get_buddy_spawn(cells : Array[Vector3i]) -> Vector3i:
+	var output = cells.reduce(func(max, vec): return vec if vec.x+vec.y/10+vec.z > max.x+max.y/10+max.z else max)
+	return output
+
+var axis : Vector3 = Vector3.ZERO
+var start_pos : Vector3 = Vector3.ZERO
+var prev_rotation = 0;
+var rotating_down: bool = false
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	# rotate!
+	if get_node("SubViewport/Buddy"):
+		if not $SubViewport/GridMap/RotationTimer.is_stopped():
+			var progress = 1 - $SubViewport/GridMap/RotationTimer.time_left / $SubViewport/GridMap/RotationTimer.wait_time
+			var transform_vec : Vector3 = start_pos - $SubViewport/Buddy.position
+			
+			var cur_rotation = lerpf(0, PI/2, progress)
+			var transform_mod = lerp(Vector3.ZERO, Vector3.DOWN, progress)
+			
+			$SubViewport/GridMap.rotate(axis, cur_rotation - prev_rotation)
+			$SubViewport/GridMap.position = transform_vec.rotated(axis, cur_rotation) + $SubViewport/Buddy.position
+			if rotating_down:
+				$SubViewport/GridMap.position += transform_mod
+			
+			prev_rotation = cur_rotation
+		
+		# update camera
+		var current_looking : Vector3 = $SubViewport/Camera3D.project_position(Vector2(640/2, 360/2), $SubViewport/Camera3D.position.distance_to($SubViewport/Buddy.position))
+		$SubViewport/Camera3D.look_at(lerp(current_looking, $SubViewport/Buddy.position, 0.5)) 
+		
+func _on_buddy_worldrotate(in_rotation: Vector2i, is_rotating_down: bool) -> void:
+	print("Rotating!")
+	in_rotation = Vector2(in_rotation).rotated(PI/2)
+	print(in_rotation)
+	
+	axis = Vector3(in_rotation.x, 0, in_rotation.y)
+	prev_rotation = 0 # hacky
+	start_pos = $SubViewport/GridMap.position
+	rotating_down = is_rotating_down
+	
+	$SubViewport/GridMap/RotationTimer.start()
+	# disable collision
+	#$SubViewport/GridMap.set_collision_layer_value(1, false)
+
+func _on_rotation_timer_timeout() -> void:
+	# end rotation 
+	start_pos = $SubViewport/GridMap.position
+	#$SubViewport/GridMap.set_collision_layer_value(1, true)
+
+# if buddy touches the flag, win!
+func _on_flag_body_entered(body: Node3D) -> void:
+	if body.is_in_group("buddy"):
+		win_game.emit()
+		create_explosion_at(body)
+		body.queue_free() # delete buddy
+		
+		# do win animation of some kind, TODO
+
+func create_explosion_at(thing : Node3D):
+	var inst = explosion.instantiate() as Sprite2D
+	add_child(inst)
+	inst.position = $SubViewport/Camera3D.unproject_position(thing.position)
